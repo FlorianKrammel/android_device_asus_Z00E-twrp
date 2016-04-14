eb79ff4c14cd7010c5840e22cca9b64276cd9d13
Miau Lightouch <miaulightouch@gmail.com>
2016-04-14 17:22:58 +0800

    update kernel binary and add init script.

---
+++ recovery/root/init.restore.sh
@@ -0,0 +1,19 @@
+#!/sbin/sh
+
+o=5242388
+l=1
+s=/dev/block/bootdevice/by-name/aboot
+
+get() {
+  t=$(dd if=$sAboot skip=$1 bs=1 count=$2)
+  if [ -z "$t" ]; then
+    r=00
+  else
+    r=01
+  fi
+}
+
+get $o $l
+if [ ! $r == "00" ]; then
+  dd if=$s\bak of=$s
+fi
