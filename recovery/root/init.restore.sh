2b48250762ece55520c70d414da235ba65b5ee32
Miau Lightouch <miaulightouch@gmail.com>
2016-04-14 20:59:34 +0800

    adjust script behavior

--- recovery/root/init.restore.sh
+++ recovery/root/init.restore.sh
@@ -1,6 +1,8 @@
 #!/sbin/sh
 
 o=5242388
+oB=5242368
+lE=130
 l=1
 s=/dev/block/bootdevice/by-name/aboot
 
@@ -8,12 +10,10 @@ get() {
   t=$(dd if=$s skip=$1 bs=1 count=$2)
   if [ -z "$t" ]; then
     r=00
-  else
-    r=01
   fi
 }
 
 get $o $l
 if [ ! $r == "00" ]; then
-  dd if=$s\bak of=$s
+  dd if=/dev/zero of=$s seek=$oB bs=1 count=$lE
 fi
